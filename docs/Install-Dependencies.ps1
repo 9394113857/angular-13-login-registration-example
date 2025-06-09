cebb508dbf8608492ffab3687b29c43e856bdad4
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2025-06-09 08:37:47 +0530

    From Laptop

---
+++ docs/Install-Dependencies.ps1
@@ -0,0 +1,5 @@
+// Installs project dependencies while bypassing peer dependency conflicts.
+// Use this command if you encounter errors related to incompatible peer dependencies during installation.
+npm install --legacy-peer-deps
+
+ 
